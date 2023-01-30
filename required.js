7d433afffe5d4c2bd61af2963a3fa9c1ad5d6363
Jack Kennedy <114001547+ponyfan88@users.noreply.github.com>
2023-01-30 12:18:56 -0500

    update to local version

--- required.js
+++ required.js
@@ -1,11 +1,11 @@
-
+// on page load
 document.addEventListener("DOMContentLoaded", function () {
-    var hideme = document.getElementById("hideme");
-    hideme.style.display = "none";
+    var hideme = document.getElementById("hideme"); // find the div that has the id "hideme"
+    hideme.style.display = "none"; // set its display style to "none" (hiding its contents)
 });
 
-function show() {
-    hideme.style.display = "contents";
-    document.getElementById("epic music").play();
-    document.getElementById("loadbutton").style.display = "none";
+function show() { // "show" function (called when we click or "load page" button)
+    hideme.style.display = "contents"; // show our div with the id "hideme"
+    document.getElementById("epic music").play(); // start playing our epic music
+    document.getElementById("loadbutton").style.display = "none"; // hide our "load page" button
 }
